d825f90cc03b5bf4093ba036cb20dc447d96c6cc
shimamoto <shimamoto@apache.org>
2018-04-24 14:26:46 +0900

    Add template for generating an e-com recommendation engine.

---
+++ src/main/scaffolds/e-recommender/src/main/scala/$package$/Engine.scala
@@ -0,0 +1,31 @@
+package $package$
+
+import org.apache.predictionio.controller.EngineFactory
+import org.apache.predictionio.controller.Engine
+
+case class Query(
+  user: String,
+  num: Int,
+  categories: Option[Set[String]],
+  whiteList: Option[Set[String]],
+  blackList: Option[Set[String]]
+)
+
+case class PredictedResult(
+  itemScores: Array[ItemScore]
+)
+
+case class ItemScore(
+  item: String,
+  score: Double
+)
+
+object ECommerceRecommendationEngine extends EngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map("ecomm" -> classOf[ECommAlgorithm]),
+      classOf[Serving])
+  }
+}
